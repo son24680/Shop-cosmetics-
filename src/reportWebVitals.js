bbc1b97bc7c05def663937f90d298af89d13186c
son24680 <thaison2468024@gmail.com>
2024-03-24 14:02:30 +0700

    reactjs_ecommerce

---
+++ src/reportWebVitals.js
@@ -0,0 +1,16 @@
+import { getFormSubmissionInfo } from 'react-router-dom/dist/dom';
+import { getTokenLogin } from './store/authSlice';
+
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
